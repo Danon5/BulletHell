a44dbc6465901ee7c49449b447d8f468247c28c7
Danon5 <danonfive@gmail.com>
2022-06-15 01:23:08 -0700

    - initial commit

---
+++ Assets/_Project/Codebase/Testing.cs
@@ -0,0 +1,23 @@
+﻿using BulletHell.ECS.Components.Singletons;
+using Unity.Entities;
+using UnityEngine;
+
+namespace BulletHell
+{
+    public sealed class Testing : MonoBehaviour
+    {
+        private EntityManager _entityManager;
+
+        private void Start()
+        {
+            _entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+            Entity gameDataEntity = _entityManager.CreateEntityQuery(
+                typeof(GameDataSingletonComponent)).GetSingletonEntity();
+
+            GameDataSingletonComponent gameData = _entityManager.GetComponentData<GameDataSingletonComponent>(gameDataEntity);  
+
+            _entityManager.Instantiate(gameData.playerPrefab);
+        }
+    }
+}
